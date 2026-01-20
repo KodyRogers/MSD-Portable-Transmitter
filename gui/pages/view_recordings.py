551c2ac255ad4f70c2528cf1d9ac5ed56a46e934
KodyRogers <124380294+KodyRogers@users.noreply.github.com>
2026-01-20 13:41:50 -0500

    Update view_recordings.py

--- gui/pages/view_recordings.py
+++ gui/pages/view_recordings.py
@@ -1,10 +1,45 @@
 import tkinter as tk
+from tkinter import font
+
+from gui.pages.helper.load_files import load_files
 
 class ViewRecordings(tk.Frame):
+    
     def __init__(self, parent, app):
         super().__init__(parent)
 
-        tk.Label(self, text="View Recordings Page", font=("Arial", 14)).pack(pady=10)
+        my_font = font.Font(family="Times New Roman", size=16)
+
+        tk.Label(self, text="View Recordings Page", font=my_font).pack(pady=10)
+
+        tk.Label(
+            self, text="Select Messages (order shown on right):", font=my_font
+        ).pack(pady=10)
+
+        # Main Frame with two listboxes
+        main_frame = tk.Frame(self)
+        main_frame.pack(padx=20, pady=10, fill='both', expand=True)
+
+        # ---- LEFT: Available messages ----
+        left_frame = tk.Frame(main_frame)
+        left_frame.pack(side=tk.LEFT, padx=10, fill='both', expand=True)
+
+        tk.Label(left_frame, text="Available Messages", font=my_font).pack()
+
+        left_scroll = tk.Scrollbar(left_frame, orient=tk.VERTICAL)
+        left_scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
+        left_list = tk.Listbox(
+            left_frame, font=my_font, yscrollcommand=left_scroll.set,
+            height=12, selectmode=tk.SINGLE
+        )
+        files = load_files()
+        for m in files:
+            message = f"{m['modified']} | duration: {int(m['duration'])} | {m['filename']}"
+            left_list.insert(tk.END, message)
+
+        left_list.pack(side=tk.LEFT, fill='both', expand=True)
+        left_scroll.config(command=left_list.yview)
 
         tk.Button(self, text="Back to Main Menu",
                   command=lambda: app.show_frame("MainMenu")).pack(pady=5)
